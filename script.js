3ba452cbbee6793c161cbb07feeabe33455594f2
JuanJoseGomezR <juangomez0149@gmail.com>
2022-05-27 09:34:31 -0500

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+import hamburguerMenu from "./js/hamburger.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    hamburguerMenu(".panel-btn", ".panel");
+});
+
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
@@ -7,3 +15,4 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-300px"
 }
+
